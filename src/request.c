65391ec956f48f0e6f8a6163d8878b8062480656
perrynzhou <perrynzhou@gmail.com>
2020-02-17 14:26:05 +0800

    add response for request

--- src/request.c
+++ src/request.c
@@ -13,28 +13,32 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 #define REQUEST_READ_BUFFER_LENGTH (1024 * 1024)
-int request_put_object_init(int sock, const char *path)
+int request_put_object_init(response_ack *ack, int sock, const char *path)
 {
+  bool is_success = false;
+  request_put_object *req = NULL;
   if (sock != -1 && path != NULL)
   {
     struct stat st;
     if (stat(path, &st) != -1)
     {
       size_t req_len = REQUEST_READ_BUFFER_LENGTH + sizeof(request_put_object);
-      request_put_object *req = (request_put_object *)calloc(req_len, sizeof(char));
+      req = (request_put_object *)calloc(req_len, sizeof(char));
       assert(req != NULL);
       uint8_t uid[BUCKET_OBJECT_UID_SZ] = {'\0'};
       slice name;
       parse_file_name(&name, path);
       req->data_length = st.st_size;
       srand((int)time(NULL));
-      req->req_id = rand();
-      req->req_type = REQ_PUT_OBJECT;
+      req->head.req_id = rand();
+      req->head.req_type = REQ_PUT_OBJECT;
       strncpy((char *)&req->name, slice_value(&name), slice_size(&name));
       md5_file(path, (char *)&uid);
       slice_deinit(&name);
@@ -48,12 +52,59 @@ int request_put_object_init(int sock, const char *path)
         if (write_n(sock, req, req_len) != req_len)
         {
           fprintf(stdout, "write failed\n");
-          break;
+          is_success = false;
+          goto out;
         }
       }
-      close(sock);
-      return 0;
+      is_success = true;
+      if (is_success)
+      {
+        read_n(fd, ack, sizeof(*ack));
+      }
     }
   }
-  return -1;
+out:
+  if (req != NULL)
+  {
+    free(req);
+  }
+  if (sock != -1)
+  {
+    close(sock);
+  }
+  return (is_success) ? 0 : -1;
+}
+int request_put_bucket_init(response_ack *ack, int sock, const char *name)
+{
+  bool is_success = false;
+  request_put_bucket *req = NULL;
+  if (sock != -1 && name != NULL)
+  {
+    req = (request_put_bucket *)calloc(1, sizeof(request_put_bucket));
+    assert(req != NULL);
+    srand((int)time(NULL));
+    req->head.req_id = rand();
+    req->head.req_type = REQ_PUT_OBJECT;
+    strncpy((char *)&req->name, name, strlen(name));
+    if (read_n(sock, req, sizeof(*req)) != sizeof(*req))
+    {
+      is_success = false;
+      goto out;
+    }
+    is_success = true;
+    if (is_success)
+    {
+      read_n(sock, ack, sizeof(*ack));
+    }
+  }
+out:
+  if (req != NULL)
+  {
+    free(req);
+  }
+  if (sock != -1)
+  {
+    close(sock);
+  }
+  return (is_success) ? 0 : -1;
 }
--- src/request.h
+++ src/request.h
@@ -9,18 +9,36 @@
 #define _REQUEST_H
 #include <stdint.h>
 #define BUCKET_OBJECT_UID_SZ  (32)
-#define BUCKET_OBJECT_NAME_SZ   (255)
+#define BUCKET_BUFFER_LENGTH   (255)
  enum  request_type{
    REQ_PUT_OBJECT=0,
    REQ_DEL_OBJECT,
+   REQ_PUB_BUCKET,
 };
+typedef struct request_head_t {
+     uint8_t req_type;
+     uint32_t req_id;
+}request_head;
+typedef struct response_head_t {
+    uint8_t req_type;
+     uint32_t req_id;
+     char    name[BUCKET_BUFFER_LENGTH];
+}response_head;
+typedef struct response_ack_t{
+   response_head ack;
+   int  ret;
+}response_ack;
 typedef struct request_put_object_t {
-   uint8_t req_type;
-   uint32_t req_id;
-   char    name[BUCKET_OBJECT_NAME_SZ];
+   request_head  head;
+   char    name[BUCKET_BUFFER_LENGTH];
    uint8_t uid[BUCKET_OBJECT_UID_SZ];
    uint64_t data_length;
    char    data[];
 }request_put_object;
-int request_put_object_init(int sock,const char *path);
+typedef struct request_put_bucket_t {
+     request_head  head;
+       char    name[BUCKET_BUFFER_LENGTH];
+}request_put_bucket;
+int  request_put_object_init(response_ack *ack,int sock,const char *path);
+int request_put_bucket_init(response_ack *ack,int sock,const char *name);
 #endif
